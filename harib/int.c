58d0142ad8abc2bcd61f9e2b3090d69d9d47ee0c
Jung's Laptop <sublime718@hotmail.com>
2014-12-24 21:25:20 +0900

    enhanced FIFO Buffer handling

--- harib/int.c
+++ harib/int.c
@@ -34,9 +34,13 @@ void inthandler21(int *esp)
 	io_out8(PIC0_OCW2, 0x61);	/* IRQ-01 접수 완료를 PIC에 통지 */
 	data = io_in8(PORT_KEYDAT);
 
-	if (keybuf.next < 32) {
-		keybuf.data[keybuf.next] = data;
-		keybuf.next++;
+	if (keybuf.len < 32) {
+		keybuf.data[keybuf.next_w] = data;
+		keybuf.len++;
+		keybuf.next_w++;
+		if (keybuf.next_w == 32) {
+			keybuf.next_w = 0;
+		}
 	}
 
 	return;
