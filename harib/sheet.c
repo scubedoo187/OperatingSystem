2941e94670f3047205db07e07ece09ba02d3ec04
Jung's Laptop <sublime718@hotmail.com>
2015-01-04 23:47:26 +0900

    enhanced window handling

--- harib/sheet.c
+++ harib/sheet.c
@@ -12,6 +12,13 @@ struct SHTCTL *shtctl_init(struct MEMMAN *memman, unsigned char *vram, int xsize
 	if (ctl == 0) {
 		goto err;
 	}
+	/* map에 의해 추가된 부분 */
+	ctl->map = (unsigned char *)memman_alloc_4k(memman, xsize * ysize);
+	if (ctl->map == 0) {
+		memman_free_4k(memman, (int)ctl, sizeof(struct SHTCTL));
+		goto err;
+	}
+
 	ctl->vram = vram;
 	ctl->xsize = xsize;
 	ctl->ysize = ysize;
@@ -71,7 +78,8 @@ void sheet_updown(struct SHEET *sht, int height)
 				ctl->sheets[h]->height = h;
 			}
 			ctl->sheets[height] = sht;
-			sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, height + 1);
+			sheet_refreshmap(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, height + 1);
+			sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, height + 1, old);
 		} else {		/* 비표시화 */
 			if (ctl->top > old) {
 				/* 위에 있는 것을 하나씩 내린다. */
@@ -82,7 +90,8 @@ void sheet_updown(struct SHEET *sht, int height)
 			}
 			ctl->top--;		/* 표시 중의 레이어가 1개 줄어들게되므로, 맨위의 높이가 줄어든다. */
 		}
-		sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, 0);
+		sheet_refreshmap(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, 0);
+		sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, 0, old - 1);
 	} else if (old < height) {	/* 이전보다 높아진다. */
 		if (old >= 0) {
 			/* 사이에 있는 것을 하나씩 내린다. */
@@ -100,7 +109,8 @@ void sheet_updown(struct SHEET *sht, int height)
 			ctl->sheets[height] = sht;
 			ctl->top++;		/* 표시 중의 레이어가 1개 증가하므로, 맨 위의 높이가 증가한다. */
 		}
-		sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, height);
+		sheet_refreshmap(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, height);
+		sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, height, height);
 	}
 	return;
 }
@@ -109,24 +119,25 @@ void sheet_refresh(struct SHEET *sht, int bx0, int by0, int bx1, int by1)
 {
 	if (sht->height >= 0) {
 		/* 만약 표시 중이라면, 새로운 레이어 정보에 따라 화면을 다시 그린다. */
-		sheet_refreshsub(sht->ctl, sht->vx0 + bx0, sht->vy0 + by0, sht->vx0 + bx1, sht->vy0 + by1, sht->height);
+		sheet_refreshsub(sht->ctl, sht->vx0 + bx0, sht->vy0 + by0, sht->vx0 + bx1, sht->vy0 + by1, sht->height, sht->height);
 	}
 	return;
 }
 
-void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0)
+void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0, int h1)
 {
 	int h, bx, by, vx, vy, bx0, by0, bx1, by1;
-	unsigned char *buf, c, *vram = ctl->vram;
+	unsigned char *buf, c, *vram = ctl->vram, *map = ctl->map, sid;
 	struct SHEET *sht;
 	/* refresh 범위가 화면 밖에 있으면 보정 */
 	if (vx0 < 0) { vx0 = 0; }
 	if (vy0 < 0) { vy0 = 0; }
 	if (vx1 > ctl->xsize) { vx1 = ctl->xsize; }
 	if (vy1 > ctl->ysize) { vy1 = ctl->ysize; }
-	for (h = h0; h <= ctl->top; h++) {
+	for (h = h0; h <= h1; h++) {
 		sht = ctl->sheets[h];
 		buf = sht->buf;
+		sid = sht - ctl->sheets0;
 		/* vx0~vy1 을 사용하여 bx0~by1을 역산한다 */
 		bx0 = vx0 - sht->vx0;
 		by0 = vy0 - sht->vy0;
@@ -136,15 +147,46 @@ void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, in
 		if (by0 < 0) { by0 = 0; }
 		if (bx1 > sht->bxsize) { bx1 = sht->bxsize; }	/* 겹치기의 다른 부분에 관한 처리 */
 		if (by1 > sht->bysize) { by1 = sht->bysize; }		
-		for (by = 0; by < sht->bysize; by++) {
+		for (by = by0; by < by1; by++) {
 			vy = sht->vy0 + by;
-			for (bx = 0; bx < sht->bxsize; bx++) {
+			for (bx = bx0; bx < bx1; bx++) {
 				vx = sht->vx0 + bx;
-				if (vx0 <= vx && vx < vx1 && vy0 <= vy && vy < vy1) {
-					c = buf[by * sht->bxsize + bx];
-					if (c != sht->col_inv) {
-						vram[vy * ctl->xsize + vx] = c;
-					}
+				if (map[vy * ctl->xsize + vx] == sid) {
+					vram[vy * ctl->xsize + vx] = buf[by * sht->bxsize + bx];
+				}
+			}
+		}
+	}
+	return;
+}
+
+void sheet_refreshmap(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0)
+{
+	int h, bx, by, vx, vy, bx0, by0, bx1, by1;
+	unsigned char *buf, sid, *map = ctl->map;
+	struct SHEET *sht;
+	if (vx0 < 0) { vx0 = 0; }
+	if (vy0 < 0) { vy0 = 0; }
+	if (vx1 > ctl->xsize) { vx1 = ctl->xsize; }
+	if (vy1 > ctl->ysize) { vy1 = ctl->ysize; }
+	for (h = h0; h <= ctl->top; h++) {
+		sht = ctl->sheets[h];
+		sid = sht - ctl->sheets0;	/* 번지를 빼서 그것을 레이어의 번호로 사용 */
+		buf = sht->buf;
+		bx0 = vx0 - sht->vx0;
+		by0 = vy0 - sht->vy0;
+		bx1 = vx1 - sht->vx0;
+		by1 = vy1 - sht->vy0;
+		if (bx0 < 0) { bx0 = 0; }
+		if (by0 < 0) { by0 = 0; }
+		if (bx1 > sht->bxsize) { bx1 = sht->bxsize; }
+		if (by1 > sht->bysize) { by1 = sht->bysize; }
+		for (by = by0; by < by1; by++) {
+			vy = sht->vy0 + by;
+			for (bx = bx0; bx < bx1; bx++) {
+				vx = sht->vx0 + bx;
+				if (buf[by * sht->bxsize + bx] != sht->col_inv) {
+					map[vy * ctl->xsize + vx] = sid;
 				}
 			}
 		}
@@ -154,12 +196,15 @@ void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, in
 
 void sheet_slide(struct SHEET *sht, int vx0, int vy0)
 {
+	struct SHTCTL *ctl = sht->ctl;
 	int old_vx0 = sht->vx0, old_vy0 = sht->vy0;
 	sht->vx0 = vx0;
 	sht->vy0 = vy0;
 	if (sht->height >= 0) {		/* 만약 표시 중이라면 */
-		sheet_refreshsub(sht->ctl, old_vx0, old_vy0, old_vx0 + sht->bxsize, old_vy0 + sht->bysize, 0);		/* 새로운 레이어 정보에 따라 화면을 다시 그린다. */
-		sheet_refreshsub(sht->ctl, vx0, vy0, vx0 + sht->bxsize, vy0 + sht->bysize, sht->height);
+		sheet_refreshmap(ctl, old_vx0, old_vy0, old_vx0 + sht->bxsize, old_vy0 + sht->bysize, 0);
+		sheet_refreshmap(ctl, vx0, vy0, vx0 + sht->bxsize, vy0 + sht->bysize, sht->height);
+		sheet_refreshsub(sht->ctl, old_vx0, old_vy0, old_vx0 + sht->bxsize, old_vy0 + sht->bysize, 0, sht->height - 1);		/* 새로운 레이어 정보에 따라 화면을 다시 그린다. */
+		sheet_refreshsub(sht->ctl, vx0, vy0, vx0 + sht->bxsize, vy0 + sht->bysize, sht->height, sht->height);
 	}
 	return;
 }
@@ -172,3 +217,4 @@ void sheet_free(struct SHEET *sht)
 	sht->flags = 0;		/* 미사용 마크 */
 	return;
 }
+
