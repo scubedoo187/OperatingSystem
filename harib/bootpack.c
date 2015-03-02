d80de7f288c7d42b63add7847c77c6feba4487fc
YoungGyun Jung <sublime718@hotmail.com>
2015-03-02 13:19:38 +0900

    ~harib14c

--- harib/bootpack.c
+++ harib/bootpack.c
@@ -5,6 +5,7 @@
 
 void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c);
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act);
+void make_wtitle8(unsigned char *buf, int xsize, char *title, char act);
 void putfonts8_asc_sht(struct SHEET *sht, int x, int y, int c, int b, char *s, int l);
 void task_b_main(struct SHEET *sht_back);
 void console_task(struct SHEET *sheet);
@@ -203,6 +204,10 @@ void HariMain(void)
 	}
 }
 
+void make_wtitle8(unsigned char *buf, int xsize, char *title, char act)
+{
+	
+}
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act)
 {
 	static char closebtn[14][16] = {
