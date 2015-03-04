72b0f3a054c165cd656f849bc8a8b68e676169a2
JungYG <sublime718@hotmail.com>
2015-03-04 15:12:54 +0900

    ~harib15a

--- harib/bootpack.c
+++ harib/bootpack.c
@@ -288,9 +288,11 @@ void HariMain(void)
                     }
                 }
                 timer_settime(timer, 50);
-                boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x,
-                        28, cursor_x + 7, 43);
-                sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
+                if (cursor_c >= 0) {
+                    boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x,
+                            28, cursor_x + 7, 43);
+                    sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
+                }
             }
         }
     }
